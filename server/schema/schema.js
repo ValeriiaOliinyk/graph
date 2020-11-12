eba9c7b592e851d41099fd719776c4a0463111b6
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-11-12 14:03:47 +0200

    added function for query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,21 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+
+const movies = [
+  { id: "1", name: "Kill Bill", genre: "drama, thriller" },
+  { id: "2", name: "Mummy", genre: "fantasy, horror, thriller" },
+  {
+    id: 3,
+    name: "Harry Potter and the Chamber of Secrets",
+    genre: "fantasy",
+  },
+  { id: 4, name: "Game of thrones ", genre: "fantasy, thriller" },
+];
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
@@ -15,8 +26,10 @@ const Query = new GraphQLObjectType({
   fields: {
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },
-      resolve(parent, args) {},
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return movies.find((movie) => movie.id == args.id);
+      },
     },
   },
 });
